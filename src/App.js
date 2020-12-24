6953dde8b6004579be68b5442f786a8185588f7a
Suraj Hanvate <surajhanvate999@gmail.com>
2020-12-25 00:13:28 +0530

    React Todo App

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+
+  return (
+    <div className="App">
+      <header>
+        <h1>My Todo List</h1>
+      </header>
+      <Form inputText={inputText} todos={todos}
+        setTodos={setTodos} setInputText={setInputText}
+        setStatus={setStatus}   
+      />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos}/>
+    </div>
+  );
+}
+
+export default App;
